d4939917d11fdf9766b86a6cbdeefd481f5a020f
Kundan Kumar <88768757+KundanKumarRoy@users.noreply.github.com>
2023-02-17 18:17:04 +0530

    Update custome.js

--- assets/js/custome.js
+++ assets/js/custome.js
@@ -8,3 +8,33 @@ $(window).on('scroll', function () {
       $('#navbar').removeClass('sticky');
     }
   });
+
+  // item Slide
+	$('.slide_items').slick({
+	  slidesToShow:5,
+	  arrows: false,
+	  dots: false,
+	  infinite: true,
+	  speed: 500,
+	  cssEase: 'linear',
+	  autoplaySpeed: 2000,
+	  autoplay:true,
+	  responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			arrows: true,
+			dots: false,
+			slidesToShow:3
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			arrows: false,
+			dots: false,
+			slidesToShow:2
+		  }
+		}
+	  ]
+	});
